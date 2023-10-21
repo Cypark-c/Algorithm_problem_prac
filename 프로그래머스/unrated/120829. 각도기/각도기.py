611acf475041ded52f8104edcac7e72bd9ecd8ac
Cypark-c <76925535+Cypark-c@users.noreply.github.com>
2023-10-21 22:03:57 +0900

    [level unrated] Title: 각도기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.py
@@ -0,0 +1,14 @@
+def solution(angle):
+    answer=0
+    
+    if angle<90:
+        answer=1
+    elif (angle==90):
+        answer=2
+    elif (angle>90)&(angle<180):
+        answer=3
+    else:
+        answer=4
+        
+    # print(angle, answer) # for check
+    return answer
