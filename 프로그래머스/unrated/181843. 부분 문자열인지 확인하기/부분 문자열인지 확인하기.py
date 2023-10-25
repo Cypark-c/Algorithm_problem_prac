4972eea3b66caa861183c91cd8c3e5cfd6b380eb
Cypark-c <76925535+Cypark-c@users.noreply.github.com>
2023-10-25 23:40:02 +0900

    [level unrated] Title: 부분 문자열인지 확인하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.py
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.py
@@ -1,16 +1,4 @@
 def solution(my_string, target):
-    
-
-    # target의 길이가 주어질 것
-    # Apple ppl
-    # len(Appleman)=8, index 0,1,2,3,4,5,6,7 
-    # len(ppl)=3, index 0,1,2
-    
-    
-    if len(target)>len(my_string):
-        return 0
-    
-    for i in range(len(my_string)-len(target)+1):
-        if my_string[i:i+len(target)]==target:
-            return 1  
-    return 0
+    if target in my_string: 
+        return 1 
+    else: return 0
